f56568a711e83f78369809a041e608da34fc4174
seagulll <ling.d.yu@gmail.com>
2015-05-13 13:45:45 +0800

    Add insert lines and delete lines functions.

--- src/Readme(text_file_handler).txt
+++ src/Readme(text_file_handler).txt
@@ -4,12 +4,22 @@ Batch processing text documents:
 	2. Character insertion.
 	3. Character deletion.
 	4. files backup.
+	5. Delete lines.
+	6. Insert lines.
 
 Command example:
+Windows:
 python text_file_handler.py -a "replace" -b "yes" -c "C:\simon\Temp\config_file\backup" -d ".bak" -e "C:\simon\Temp\config_file\cfg" -f "*.cfg" -g "nrOfNormalSessions := 0," -i "nrOfNormalSessions := 2,"
 
+python text_file_handler.py -a "delete_line" -b "no" -e "C:\simon\Temp\titansim37_after" -f "*.cfg" -g "/template/REGISTER.txt" -k "1"
+
+Linux:
+python text_file_handler.py -a "insert_line" -b "no" -e "/home/ttcn3/elingyu/scripts/titansim_scripts" -f "*.cfg" -g "trafficSpecific := {" -j "src.txt"
+
+python text_file_handler.py -a "delete_line" -b "no" -e "/home/ttcn3/elingyu/scripts/titansim_scripts" -f "*.cfg" -g "/template/REGISTER.txt" -k "1"
+
 Parameters:
--a: Action.(replace; insert; delete)
+-a: Action.(replace; insert; delete; insert_line; delete_line)
 -b: Backup the old files.(yes; no)
 -c: Backup directory.
 -d: Backup file suffix.
@@ -17,3 +27,7 @@ Parameters:
 -f: file suffix.
 -g: Origin characters.
 -i: target characters.
+-j: target characters file.
+-k: delete line number.
+
+
--- src/text_file_handler.py
+++ src/text_file_handler.py
@@ -16,11 +16,11 @@ def parse_args():
 This is the txt file handler.
 Run it like this:
 
-    python text_file_handler.py -a "replace" -b "yes" -c "C:\simon\Temp\config_file\backup" -e "C:\simon\Temp\config_file\cfg" -f "*.cfg" -g love -i "hate"
+    python text_file_handler.py -a "replace" -b "yes" -c "C:\simon\Temp\config_file\backup" -e "C:\simon\Temp\config_file\cfg" -f "*.cfg" -g hate -i "love"
     
 """
     parser = optparse.OptionParser(usage)
-    parser.add_option("-a", "--do", dest="do", type="string", help="replace; insert; delete", default="replace")
+    parser.add_option("-a", "--do", dest="do", type="string", help="replace; insert; delete; insert_line; delete_line", default="replace")
     
     parser.add_option("-b", "--backup", dest="backup", type="string", help="yes; no", default="yes")
     parser.add_option("-c", "--backup directory", dest="backup_directory", type="string", help="backup directory", default="c:")
@@ -30,6 +30,9 @@ Run it like this:
     parser.add_option("-g", "--origin characters", dest="origin_characters", type="string", help="origin characters", default="")
     
     parser.add_option("-i", "--target characters", dest="target_characters", type="string", help="target characters", default="")    
+    
+    parser.add_option("-j", "--target_characters_file", dest="target_characters_file", type="string", help="target_characters_file", default="")    
+    parser.add_option("-k", "--del_line_number", dest="del_line_number", type="string", help="del_line_number", default="")    
 
     (options, _) = parser.parse_args()
     
@@ -44,6 +47,10 @@ Run it like this:
         print "You are trying to insert the %s behind %s !" %(options.target_characters, options.origin_characters)
     elif options.do == "delete":
         print "You are trying to delete the %s !" %(options.origin_characters)
+    elif options.do == "insert_line":
+        print "You are trying to insert line !"
+    elif options.do == "delete_line":
+        print "You are trying to delete line !"
     else:
         print "The do %s is illegal!" %(options.do)
         parser.exit()
@@ -60,7 +67,7 @@ Run it like this:
         print "The backup number %s is illegal!" %(options.backup)
         parser.exit()
     
-    return options.do, options.backup, options.backup_directory, options.origin_directory, options.origin_files, options.origin_characters, options.target_characters
+    return options.do, options.backup, options.backup_directory, options.origin_directory, options.origin_files, options.origin_characters, options.target_characters, options.target_characters_file, options.del_line_number
 
 
 def backup_file(origin_directory, file_name, backup_directory):
@@ -86,8 +93,54 @@ def delete_characters(origin_directory,file_name, origin_characters):
     for s in lines:
         fp.write(s.replace(origin_characters, ""))
     fp.close()
+    
+def insert_line(origin_directory, file_name, origin_characters, target_characters_file):
+    fp_orig = open(origin_directory + "//" + file_name, 'r')    
+    lines = fp_orig.readlines()
+    fp_orig.close()
+    
+    fp_target =  open(origin_directory + "//" + target_characters_file, 'r') 
+    target_lines = fp_target.readlines()
+    fp_target.close()
+    target_str = ''
+    
+    for tmp in target_lines:
+        target_str += tmp
+    
+    all_text = ''
+    for s in lines:
+        if s.find(origin_characters) != -1:
+            s += target_str
+        all_text += s
+        
+    fp = open(origin_directory + "//" + file_name, 'w')
+    fp.write(all_text)
+    fp.close()
 
-def text_file_handler(do, backup, backup_directory, origin_directory, origin_files, origin_characters, target_characters):
+def delete_line(origin_directory, file_name, origin_characters, del_line_number):
+    fp_orig = open(origin_directory + "//" + file_name, 'r')    
+    lines = fp_orig.readlines()
+    fp_orig.close()
+    
+    all_text = ''
+    del_counter = 0
+    for s in lines:
+        if s.find(origin_characters) != -1:
+            del_counter = int(del_line_number)-1
+            continue
+        
+        if del_counter != 0:
+            del_counter -= 1
+            continue
+        
+        all_text += s
+        
+    fp = open(origin_directory + "//" + file_name, 'w')
+    fp.write(all_text)
+    fp.close()
+
+
+def text_file_handler(do, backup, backup_directory, origin_directory, origin_files, origin_characters, target_characters, target_characters_file, del_line_number):
     file_list = os.listdir(origin_directory)
     file_number = len(file_list)
     org_file_list = []
@@ -110,6 +163,12 @@ def text_file_handler(do, backup, backup_directory, origin_directory, origin_fil
             
         if do == "delete":
             delete_characters(origin_directory, org_file_list[i], origin_characters)
+        
+        if do == "insert_line":
+            insert_line(origin_directory, org_file_list[i], origin_characters, target_characters_file)
+
+        if do == "delete_line":
+            delete_line(origin_directory, org_file_list[i], origin_characters, del_line_number)
         
 
 if __name__ == '__main__':
